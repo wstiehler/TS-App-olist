28f65c41c0b609a422f1408568bfa8c94e7ccb6d
william villani stiehler <william.stiehler@olist.com>
2021-06-18 17:39:42 -0300

    Upload create migrations and base project

--- register_car/models.py
+++ register_car/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class RegisterCar(models.Model):
+
+    name = models.CharField(max_length=20)
+    type_car = models.CharField(max_length=20)
+    plaque = models.CharField(max_length=8)
+    value = models.FloatField()
+
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.type_car} - {self.plaque} - {self.value}'
