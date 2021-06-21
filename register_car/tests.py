3188def6d031b3898d2923bb4f86d4a39903eb71
william villani stiehler <william.stiehler@olist.com>
2021-06-20 23:03:59 -0300

    Upload increment tests initial in project AppOlist

--- register_car/tests.py
+++ register_car/tests.py
@@ -1,3 +1,17 @@
+from register_car.models import RegisterCar
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self) -> None:
+        register_car = RegisterCar.objects.create(
+            name='TestName', type_car='TestType', plaque='TestPlaque', value=700)
+        register_car.save()
+        self.response_get = self.client.get('/registercar/list')
+    
+    def test_route_list_all_returning_status_code(self):
+        self.assertEqual(self.response_get.status_code, 200)
+    
+    def test_route_list_all_returning_is_valid_template(self):
+        self.assertTemplateUsed(self.response_get, 'register_car_view.html')
