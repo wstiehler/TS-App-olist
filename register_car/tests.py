a02bd203fe4421afbbcd82fae6fb4b8a16767ee0
william villani stiehler <william.stiehler@olist.com>
2021-06-21 16:13:38 -0300

    Update increment tests

--- register_car/tests.py
+++ register_car/tests.py
@@ -1,3 +1,4 @@
+from unittest import result
 from django.test import TestCase
 from django.shortcuts import resolve_url
 
@@ -11,16 +12,35 @@ class TestViewsGet(TestCase):
     def setUp(self) -> None:
         self.response = self.client.get(resolve_url('registercar'))
 
-    def test_route_list_all_returning_status_code_200(self):
-        self.assertEqual(200, self.response.status_code)
+    def test_route_returning_status_code_200(self):
+        self.assertEqual(self.response.status_code, 200)
     
-    def test_route_list_all_returning_is_valid_template(self):
+    def test_route_returning_is_valid_template(self):
         self.assertTemplateUsed(self.response, 'register_car.html')
 
 
-
-
-class TestViewCreate(TestCase):
+class TestViewReturningRouteTemplate(TestCase):
     def setUp(self) -> None:
-        register_car = dict(name='test', type_car='test', plaque='test', value=700)
-        self.response = self.client.post(resolve_url('registercar',register_car))
+        register = RegisterCar.objects.create(name='test', type_car='test',
+                                                plaque='test', value=700)
+        register.save()
+
+    def test_route_view_returning_status_code_200(self):
+        self.response_get = self.client.get('/registercar/view')
+        self.assertEqual(self.response_get.status_code, 200)
+
+    def test_route_create_returning_status_code_200(self):
+        self.response_get = self.client.get('/registercar/new')
+        self.assertEqual(self.response_get.status_code, 200)
+
+    def test_route_update_without_id_returning_status_code_404(self):
+        self.response_get = self.client.get('registercar/update')
+        self.assertEqual(self.response_get.status_code, 404)
+    
+    # def test_route_update_returning_status_code_200(self):
+    #     result = RegisterCar.objects.last()
+    #     self.response_get = self.client.get(f'registercar/update/{result.id}')
+    #     self.assertEqual(self.response_get.status_code, 200)
+
+
+
