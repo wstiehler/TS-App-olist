ea851617f5f439d6b302e22cfc29568f0faf2851
william villani stiehler <william.stiehler@olist.com>
2021-06-21 23:52:35 -0300

    Update tests

--- register_car/tests.py
+++ register_car/tests.py
@@ -7,7 +7,7 @@ from register_car.forms import RegisterCarForms
 from register_car.models import RegisterCar
 
 
-class TestViewsGet(TestCase):
+class TestViewsTemplatesHtml(TestCase):
 
     def setUp(self) -> None:
         self.response = self.client.get(resolve_url('registercar'))
@@ -15,14 +15,17 @@ class TestViewsGet(TestCase):
     def test_route_returning_status_code_200(self):
         self.assertEqual(self.response.status_code, 200)
     
-    def test_route_returning_is_valid_template(self):
+    def test_route_returning_is_valid_template_register_car_html(self):
         self.assertTemplateUsed(self.response, 'register_car.html')
+    
+    def test_route_returning_is_valid_template_register_car_form_html(self):
+        self.response_template = self.client.get(resolve_url('registercar_form'))
+        self.assertTemplateUsed(self.response_template, 'register_car_form.html')
 
 
 class TestViewReturningRouteTemplate(TestCase):
     def setUp(self) -> None:
-        register = RegisterCar.objects.create(name='test', type_car='test',
-                                                plaque='test', value=700)
+        register = RegisterCar.objects.create(name='test', type_car='test',plaque='test', value=700)
         register.save()
 
     def test_route_view_returning_status_code_200(self):
@@ -32,15 +35,15 @@ class TestViewReturningRouteTemplate(TestCase):
     def test_route_create_returning_status_code_200(self):
         self.response_get = self.client.get('/registercar/new')
         self.assertEqual(self.response_get.status_code, 200)
-
-    def test_route_update_without_id_returning_status_code_404(self):
-        self.response_get = self.client.get('registercar/update')
-        self.assertEqual(self.response_get.status_code, 404)
     
-    # def test_route_update_returning_status_code_200(self):
-    #     result = RegisterCar.objects.last()
-    #     self.response_get = self.client.get(f'registercar/update/{result.id}')
-    #     self.assertEqual(self.response_get.status_code, 200)
+    def test_route_update_returning_status_code_200(self):
+        result = RegisterCar.objects.last()
+        self.response_get = self.client.get(f'/registercar/update/{result.id}')
+        self.assertEqual(self.response_get.status_code, 200)
 
+    def test_route_delete_returning_status_code_200(self):
+        result = RegisterCar.objects.last()
+        self.response_get = self.client.get(f'/registercar/delete/{result.id}')
+        self.assertEqual(self.response_get.status_code, 302)
 
 
